84dd55cb8d32f95f6e86397025da0d7c84f3f2fe
Tanis Young <tanisoliver@hotmail.co.uk>
2021-04-11 08:35:08 +0100

    set up branch for development of economy system

--- scripts/main.py
+++ scripts/main.py
@@ -3,20 +3,19 @@ import datetime
 
 from discord.ext.commands import Bot
 
-
-initial_extensions = ["cogs.simple",
-                      "cogs.economy",
-                      ]
+# Add cogs to this list as they are merged into main branch
+initial_extensions = ["cogs.economy"]
 
 
 bot = Bot(command_prefix="$")
 TOKEN = os.getenv("TOKEN")
 
-
+# Loads extensions (cogs) in from list provided
 if __name__ == "__main__":
     for extension in initial_extensions:
         bot.load_extension(extension)
 
+# Messages me on Discord when the bot is ready (faster than checking logs or typing a command)
 @bot.event
 async def on_ready():
     me = await bot.fetch_user(210454616876253184)
