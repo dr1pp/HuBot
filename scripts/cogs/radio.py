34f108699fc1aa83867d22da22abcc138d4fac67
Tanis Young <tanisoliver@hotmail.co.uk>
2021-05-01 02:59:34 +0100

    added function to get a random search term and a command to send playlist link

--- scripts/cogs/radio.py
+++ scripts/cogs/radio.py
@@ -1,7 +1,12 @@
 import discord
+import random as rand
+import spotipy as sp
+from spotipy.oauth2 import  SpotifyClientCredentials
 from discord.ext import commands
 
-VOICE_ID = 698959931549810764
+PLAYLIST_ID = "0EhIVTYDVaurXWRIXqB9At"
+
+spotify = sp.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 class RadioCog(commands.Cog):
     def __init__(self, bot):
@@ -15,7 +20,30 @@ class RadioCog(commands.Cog):
             await channel.connect()
         else:
             await ctx.send("You must be in a voice channel to use this command")
-            return
+
+
+    @commands.command(name="playlist")
+    async def playlist(self, ctx):
+        await ctx.send(f"https://open.spotify.com/playlist/{PLAYLIST_ID}")
+
+
+    @commands.command(name="random")
+    async def random(self, ctx):
+        name = await get_random_search_term()
+        await ctx.send(name)
+        
+
+async def get_random_search_term():
+    total_tracks = int(spotify.playlist(PLAYLIST_ID)['tracks']['total'])
+
+    tracks = []
+    while len(tracks) < total_tracks:
+        results = spotify.playlist_items(PLAYLIST_ID, limit=100, offset=len(tracks))
+        tracks.extend(results["items"])
+
+    track_names = [f"{track['track']['album']['artists'][0]['name']} - {track['track']['name']}" for track in
+                   tracks]
+    return rand.choice(track_names)
 
 
 def setup(bot):
