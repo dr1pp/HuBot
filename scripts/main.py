a563911e2a9d90575c1b9b479ca3ea199b8ca5c6
Tanis Young <tanisoliver@hotmail.co.uk>
2021-06-12 09:14:14 +0100

    improved bot online message

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 import warnings
-from discord import Game
+from discord import Game, Embed
 from discord_components import DiscordComponents
 from discord_slash import SlashCommand
 
@@ -10,6 +10,7 @@ from discord.ext.commands import Bot
 from sql import Database
 from cogs.games import Card
 
+init_time = datetime.datetime.now()
 
 RED_CARDS_GUILD_ID = 839985921302069248
 BLACK_CARDS_GUILD_ID = 839986047035506708
@@ -48,9 +49,12 @@ async def on_ready():
     emotes = await bot.get_guild(BLACK_CARDS_GUILD_ID).fetch_emojis()
     emotes.extend(await bot.get_guild(RED_CARDS_GUILD_ID).fetch_emojis())
     bot.playing_cards = [Card(emote) for emote in emotes if emote.name != "card_back"]
-    await bot.me.send(f"✅ Online - {datetime.datetime.now()}")
-    activity = Game("$help")
-    await bot.change_presence(activity=activity)
+    embed = Embed(title="Bot Online :white_check_mark:", colour=0x77B255)
+    embed.add_field(name="Ready at", value=str(datetime.datetime.now()), inline=True)
+    startup_time = datetime.datetime.now() - init_time
+    embed.add_field(name="Startup Duration", value=f"{startup_time.seconds}.{str(startup_time.microseconds)[:3]} seconds")
+    await bot.me.send(embed=embed)
+    await bot.change_presence(activity=Game("$help"))
 
 
 TOKEN = os.getenv("TOKEN")
