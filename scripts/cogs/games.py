789b9172146ce5554bccec865d48ae6c8db5b5ec
Tanis Young <tanisoliver@hotmail.co.uk>
2021-07-01 04:39:11 +0100

    removed edit_origin from ctx.defer in wrong context

--- scripts/cogs/games.py
+++ scripts/cogs/games.py
@@ -128,7 +128,7 @@ class SlotMachine(Game):
     async def play(self):
 
         async def spin(ctx):
-            await ctx.defer(edit_origin=True)
+            await ctx.defer()
             if self.econ.can_afford(self.user, self.bet):
                 await asyncio.sleep(3)
                 self.econ.give_money(self.user, -self.bet)
